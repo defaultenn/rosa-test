33e6be3923381895b85615f5f2e423fbf4785714
defaultenn <evgeniyabramov25@gmail.com>
2026-05-21 15:56:07 +0400

    feature: add services implementations

--- Services/InquiryRequestService.cs
+++ Services/InquiryRequestService.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using RosATest.Model.Entity;
+using RosATest.ViewModels;
 
 public interface IInquiryRequestService
 {
@@ -7,16 +9,168 @@ public interface IInquiryRequestService
 
     public Task<List<Status>> GetStatusesAsync();
     public Task<IEnumerable<InquiryRequest>> GetInquiryRequests(
-        User user
+        User user,
+        ListInquiryRequestsViewModel model
     );
 
     public Task<InquiryRequest> CreateInquiryRequestAsync(
-        User user
+        User user,
+        CreateInquiryRequestViewModel model
     );
     public Task<InquiryRequest> UpdateInquiryRequestAsync(
         User user,
-        int inquiryRequestID
+        int inquiryRequestID,
+        UpdateInquiryRequestViewModel model
     );
 
     public User GetUser(string? userID);
+}
+
+namespace RosATest.Services
+{
+    public class InquiryRequestService : IInquiryRequestService
+    {
+        private readonly AppDBContext _context;
+
+        public InquiryRequestService(AppDBContext context)
+        {
+            _context = context;
+        }
+        
+        public Task<List<Status>> GetStatusesAsync()
+        {
+            return _context.Statuses.ToListAsync();
+        }
+
+        public Task<List<InquiryType>> GetInquiryTypesAsync()
+        {
+            return _context.InquiryTypes.ToListAsync();
+        }
+
+        public async Task<InquiryRequest> CreateInquiryRequestAsync(
+            User user,
+            CreateInquiryRequestViewModel model
+        )
+        {   
+            int inquiryTypeID = 0;
+
+            if(model.InquiryTypeID == null && !string.IsNullOrEmpty(model.InquiryType))
+            {
+                // TODO: поиск схожих или идентичных вариантов перед созданием
+                InquiryType type = new InquiryType
+                {
+                    Name = model.InquiryType
+                };
+                _context.Add(type);
+                await _context.SaveChangesAsync();
+
+                inquiryTypeID = type.ID;
+            } else if(model.InquiryTypeID.HasValue)
+            {
+                inquiryTypeID = model.InquiryTypeID.Value;
+            } else
+            {
+                throw new InvalidDataException("Не указан тип справки");
+            }
+
+            var existingInquiryRequest = ScopedRequests(user)
+                .Include(ir => ir.Status)
+                .Where(ir => ir.Status!.Codename == Status.StatusCodename.WIP && ir.InquiryTypeID == inquiryTypeID)
+                .FirstOrDefault();
+
+            if(existingInquiryRequest != null)
+                throw new InvalidOperationException("Справка такого типа уже находится в работе.");
+
+            InquiryRequest request = new InquiryRequest
+            {
+                Count = model.Count,
+                Reason = model.Reason,
+                UserID = user.ID,
+                InquiryTypeID = inquiryTypeID
+            };
+            
+            // По умолчанию "В процессе"
+            // TODO: обработка отсутствия записи
+            var status = _context.Statuses.First(s => s.Codename == Status.StatusCodename.WIP);
+            request.StatusID = status.ID;
+
+            _context.Add(request);
+            await _context.SaveChangesAsync();
+            
+            return request;
+        }
+
+        public User GetUser(string? userID)
+        {   
+            if(userID == null)
+                throw new UnauthorizedAccessException("Требуется войти в систему.");
+
+            return _context.Users
+                .Include(u => u.Group)
+                .First(u => u.ID.ToString() == userID);
+        }
+
+        public IQueryable<InquiryRequest> ScopedRequests(User user)
+        {
+            var query = _context.InquiryRequests.Include(ir => ir.InquiryType);
+            switch (user.Group?.Codename)
+            {
+                case Group.GroupCodename.Accountant:
+                    //выдаем все запросы
+                    return query;
+                case Group.GroupCodename.Employee:
+                    // Выдаем собственные запросы для сотрудника
+                    return query.Where(ir => ir.UserID == user.ID);
+                default:
+                    return query;
+            }
+        }
+
+        public async Task<InquiryRequest> UpdateInquiryRequestAsync(
+            User user,
+            int inquiryRequestID,
+            UpdateInquiryRequestViewModel model
+        )
+        {   
+            // TODO: изменить подход: отправить запрос на изменение записи по ID,
+            // вместо загрузки записи в память и последующим изменением
+            var inquiryRequest = ScopedRequests(user)
+                .FirstOrDefault(ir => ir.ID == inquiryRequestID);
+
+            if(inquiryRequest == null)
+                throw new InvalidDataException("Такого запроса не существует.");
+
+            inquiryRequest.StatusID = model.StatusID;
+
+            _context.Update(inquiryRequest);
+            await _context.SaveChangesAsync();
+
+            return inquiryRequest;
+        }
+
+        public async Task<IEnumerable<InquiryRequest>> GetInquiryRequests(
+            User user, 
+            ListInquiryRequestsViewModel model
+        )
+        {
+            var query = ScopedRequests(user).Include(ir => ir.User).AsQueryable();
+
+            if(model.StatusID != null)
+            {
+                query = query.Where(ir => ir.StatusID == model.StatusID);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<InquiryRequest?> GetByIDAsync(User user, int id)
+        {
+            var inquiryRequest = ScopedRequests(user).FirstOrDefault(ir => ir.ID == id);
+            
+            if(inquiryRequest == null)
+                throw new InvalidDataException("Такого запроса не существует.");
+
+            return inquiryRequest;
+        }
+    }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using RosATest.Model.Entity;
 
 public interface IUserService
@@ -10,4 +11,85 @@ public interface IUserService
     public Task<UserSession> CreateSessionAsync(User user, string? ipAddress, string userAgent);
 
     public DateTimeOffset GetExpireSessionPolicy();
+}
+
+namespace RosATest.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDBContext _context;
+
+        public UserService(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> IdentifyAsync(string email)
+        {
+            var user = await _context.Users
+                .Include(u => u.Group)
+                .FirstOrDefaultAsync(u => u.Email.ToLower() == email.ToLower());
+
+            if(user == null)
+            {
+                throw new Exception("Неверный логин или пароль.");
+            }
+
+            return user;
+        }
+
+        public void AuthenticateAsync(User user, string password)
+        {
+            if(!BCrypt.Net.BCrypt.Verify(password, user.PasswordHash))
+            {
+                throw new Exception("Неверный логин или пароль.");
+            }
+        }
+
+        public DateTimeOffset GetExpireSessionPolicy()
+        {
+            return DateTimeOffset.UtcNow.AddDays(7);
+        }
+
+        public async Task<UserSession> CreateSessionAsync(User user, string? ipAddress, string userAgent)
+        {
+            var sessionToken = Guid.NewGuid().ToString();
+
+            UserSession userSession = new UserSession
+            {
+                UserID = user.ID,
+                IPAddress = ipAddress,
+                UserAgent = userAgent,
+                SessionToken = sessionToken,
+                ExpiresAt = GetExpireSessionPolicy().DateTime
+            };
+
+            _context.Add(userSession);
+            await _context.SaveChangesAsync();
+
+            return userSession;
+        }
+
+        public async Task<bool> LogoutAsync(string sessionToken)
+        {
+            if(string.IsNullOrEmpty(sessionToken))
+            {
+                return false;
+            }
+
+            var userSession = await _context.UserSessions
+                .FirstOrDefaultAsync(us => us.SessionToken == sessionToken);
+
+            if(userSession == null)
+                return false;
+
+            if(userSession.IsValid)
+            {
+                userSession.IsValid = false;
+                await _context.SaveChangesAsync();
+            }
+
+            return true;
+        }
+    }
 }
